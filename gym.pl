cc2cd05dd19808d88d3bd954a3f5f3568f07d0b5
Patrícia <arigonipatricia@gmail.com>
2024-04-27 17:36:19 -0300

    sistema funcionando
    * está lendo os inputs do usuário e fazendo a sugestão de acordo com as informações recebidas

--- gym.pl
+++ gym.pl
@@ -1,131 +1,113 @@
+% Tipos de grupos musculares
+grupoMuscular(superiores).
+grupoMuscular(inferiores).
+grupoMuscular(costas).
+grupoMuscular(peito).
+grupoMuscular(abdomen).
 
-# Tipos de gupros musculares
-GrupoMuscular(superiores)
-GrupoMuscular(inferiores)
-GrupoMuscular(costas)
-GrupoMuscular(peito)
-GrupoMuscular(abdomen)
+% Níveis de dificuldade
+nivel(iniciante).
+nivel(intermediario).
+nivel(avancado).
 
-# Nível de cada exercício
-Nivel("flexao plantar", iniciante)
-Nivel("agachamento", iniciante)
-Nivel("stiff", iniciante)
-Nivel("cadeira extensora", iniciante)
-Nivel("panturrilha sentado", iniciante)
-Nivel("leg press", iniciante)
-Nivel("elevacao lateral", iniciante)
-Nivel("rosca direta", iniciante)
-Nivel("rosca martelo", iniciante)
-Nivel("triceps corda", iniciante)
-Nivel("triceps na barra", iniciante)
-Nivel("flexao de bracos", iniciante)
-Nivel("remada articulada", iniciante)
-Nivel("supino reto", iniciante)
-Nivel("puxada frontal", iniciante)
-Nivel("supino vertical", iniciante)
-Nivel("pull down", iniciante)
-Nivel("leg press", intermediario)
-Nivel("agachamento na barra", intermediario)
-Nivel("cadeira extensora", intermediario)
-Nivel("flexora deitado", intermediario)
-Nivel("elevacao pelvica", intermediario)
-Nivel("elevacao de panturrilha", intermediario)
-Nivel("desenvolvimento", intermediario)
-Nivel("scott", intermediario)
-Nivel("triceps corda", intermediario)
-Nivel("meio terra", avancado)
-Nivel("remada na maquina", avancado)
-Nivel("rosca direta", intermediario)
-Nivel("triceps no banco", intermediario)
-Nivel("supino reto", intermediario)
-Nivel("supino inclinado", intermediario)
-Nivel("fly", intermediario)
-Nivel("supino vertical", intermediario)
-Nivel("crossover baixo", intermediario)
-Nivel("prancha", intermediario)
-Nivel("barra fixa", intermediario)
-Nivel("puxada frontal", intermediario)
-Nivel("serrote", intermediario)
-Nivel("remada baixa", intermediario)
-Nivel("fly invertido", intermediario)
-Nivel("agachamento livre na barra", avancado)
-Nivel("búlgaro", avancado)
-Nivel("leg press", avancado)
-Nivel("flexora deitado", avancado)
-Nivel("elevacão pelvica", avancado)
-Nivel("panturrilha sentado", avancado)
-Nivel("21 na barra w", avancado)
-Nivel("scott", avancado)
-Nivel("rosca direta", avancado)
-Nivel("triceps na barra w", avancado)
-Nivel("triceps nas paralelas", avancado)
-Nivel("triceps corda", avancado)
-Nivel("remada alta", avancado)
-Nivel("supino reto", avancado)
-Nivel("crossover", avancado)
-Nivel("supino declinado", avancado)
-Nivel("crucifixo reto", avancado)
-Nivel("abdominal na roda", avancado)
-Nivel("puxada" frontal, avancado)
-Nivel("serrote", avancado)
-Nivel("remada cavalinho", avancado)
+% Relaciona cada exercício com seu grupo muscular e nível de dificuldade
+exercicio("flexao de bracos", peito, iniciante).
+exercicio("supino reto", peito, iniciante).
+exercicio("supino reto", peito, intermediario).
+exercicio("supino reto", peito, avancado).
+exercicio("flexao plantar", inferiores, iniciante).
+exercicio("agachamento", inferiores, iniciante).
+exercicio("stiff", inferiores, iniciante).
+exercicio("cadeira extensora", inferiores, iniciante).
+exercicio("cadeira extensora", inferiores, intermediario).
+exercicio("panturrilha sentado", inferiores, iniciante).
+exercicio("panturrilha sentado", inferiores, avancado).
+exercicio("leg press", inferiores, iniciante).
+exercicio("leg press", inferiores, intermediario).
+exercicio("leg press", inferiores, avancado).
+exercicio("agachamento na barra", inferiores, intermediario).
+exercicio("flexora deitado", inferiores, intermediario).
+exercicio("flexora deitado", inferiores, avancado).
+exercicio("elevacao pelvica", inferiores, intermediario).
+exercicio("elevacao pelvica", inferiores, avancado).
+exercicio("elevacao de panturrilha", inferiores, intermediario).
+exercicio("agachamento livre na barra", inferiores, avancado).
+exercicio("bulgaro", inferiores, avancado).
+exercicio("rosca direta", superiores, iniciante).
+exercicio("rosca direta", superiores, intermediario).
+exercicio("rosca direta", superiores, avancado).
+exercicio("rosca martelo", superiores, iniciante).
+exercicio("scott", superiores, intermediario).
+exercicio("scott", superiores, avancado).
+exercicio("21 na barra w", superiores, avancado).
+exercicio("triceps corda", superiores, iniciante).
+exercicio("triceps corda", superiores, intermediario).
+exercicio("triceps corda", superiores, avancado).
+exercicio("triceps na barra", superiores, iniciante).
+exercicio("triceps no banco", superiores, intermediario).
+exercicio("triceps na barra w", superiores, avancado).
+exercicio("triceps nas paralelas", superiores, avancado).
+exercicio("elevacao lateral", superiores, iniciante).
+exercicio("desenvolvimento", superiores, intermediario).
+exercicio("remada alta", superiores, avancado).
+exercicio("supino vertical", peito, iniciante).
+exercicio("supino vertical", peito, intermediario).
+exercicio("fly", peito, intermediario).
+exercicio("supino inclinado", peito, intermediario).
+exercicio("crossover baixo", peito, intermediario).
+exercicio("crossover", peito, avancado).
+exercicio("supino declinado", peito, avancado).
+exercicio("crucifixo reto", peito, avancado).
+exercicio("remada articulada", costas, iniciante).
+exercicio("puxada frontal", costas, iniciante).
+exercicio("puxada frontal", costas, intermediario).
+exercicio("puxada frontal", costas, avancado).
+exercicio("pull down", costas, iniciante).
+exercicio("barra fixa", costas, intermediario).
+exercicio("serrote", costas, intermediario).
+exercicio("serrote", costas, avancado).
+exercicio("remada baixa", costas, intermediario).
+exercicio("fly invertido", costas, intermediario).
+exercicio("meio terra", costas, avancado).
+exercicio("remada na maquina", costas, avancado).
+exercicio("remada cavalinho", costas, avancado).
+exercicio("prancha", abdomen, intermediario).
+exercicio("abdominal na roda", abdomen, avancado).
 
-# Relaciona cada exercício com seu grupo muscular
-Exercicio("flexão de bracos", peito)
-Exercicio("supino reto", peito)
-Exercicio("flexão plantar", inferiores)
-Exercicio("agachamento", inferiores)
-Exercicio("stiff", inferiores)
-Exercicio("cadeira extensora", inferiores)
-Exercicio("panturrilha sentado", inferiores)
-Exercicio("leg press", inferiores)
-Exercicio("agachamento na barra", inferiores)
-Exercicio("flexora deitado", inferiores)
-Exercicio("elevacão pelvica", inferiores)
-Exercicio("elevacão de panturrilha", inferiores)
-Exercicio("agachamento livre na barra", inferiores)
-Exercicio("búlgaro", inferiores)
-Exercicio("rosca direta", superiores)
-Exercicio("rosca martelo", superiores)
-Exercicio("scott", superiores)
-Exercicio("21 na barra w", superiores)
-Exercicio("triceps corda", superiores)
-Exercicio("triceps na barra", superiores)
-Exercicio("triceps no banco", superiores)
-Exercicio("triceps na barra w", superiores)
-Exercicio("triceps nas paralelas", superiores)
-Exercicio("elevacão lateral", superiores)
-Exercicio("desenvolvimento", superiores)
-Exercicio("remada alta", superiores)
-Exercicio("supino vertical", peito)
-Exercicio("fly", peito)
-Exercicio("supino inclinado", peito)
-Exercicio("crossover baixo", peito)
-Exercicio("crossover", peito)
-Exercicio("supino declinado", peito)
-Exercicio("crucifixo reto", peito)
-Exercicio("remada articulada", costas)
-Exercicio("puxada frontal", costas)
-Exercicio("pull down", costas)
-Exercicio("barra fixa", costas)
-Exercicio("serrote", costas)
-Exercicio("remada baixa", costas)
-Exercicio("fly invertido", costas)
-Exercicio("meio terra", costas)
-Exercicio("remada na maquina", costas)
-Exercicio("remada cavalinho", costas)
-Exercicio("prancha", abdomen)
-Exercicio("abdominal na roda", abdomen)
+% Define o número de séries pra cada nível de dificuldade
+series(3, iniciante).
+series(3-4, intermediario).
+series(3-4, avancado).
 
-# Define o número de séries pra cada nível de dificuldade
-Series(3, iniciante)
-Series(3-4, intermediario)
-Series(3-4, avancado)
+% Define o número de repetições pra cada nível de dificuldade
+repeticoes(10-12, iniciante).
+repeticoes(8-12, intermediario).
+repeticoes(10-15, avancado).
+repeticoes(6-8, avancado).
 
-# Define o número de repetições pra cada nível de dificuldade
-Repeticoes(10-12, iniciante)
-Repeticoes(8-12, intermediario)
-Repeticoes(10-15, avancado)
-Repeticoes(6-8, avancado)
+% Predicado para recomendar exercícios com base no grupo muscular e nível de dificuldade informados
+recomendaExercicios(Nivel, GrupoMuscular) :-
+    nivel(Nivel),
+    grupoMuscular(GrupoMuscular),
+    series(Series, Nivel),
+    repeticoes(Repeticoes, Nivel),
+    findall(Exercicio, exercicio(Exercicio, GrupoMuscular, Nivel), Exercicios),
+    nl, write("-------------------------------------------------------------------------------"), nl, nl,
+    write("Exercícios de nível "), write(Nivel), write(" para o grupo muscular "), write(GrupoMuscular), nl,
+    write("Fazer "), write(Series), write(" séries de "), write(Repeticoes), write(" repetições."), nl,
+    write("Exercícios: "), nl,
+    writeList(Exercicios).
 
+% Predicado auxiliar para printar a lista de exercícios
+writeList([]).
+writeList([H|T]) :-
+    write('* '), write(H), nl,
+    writeList(T).
 
+% Predicado principal para interação com o usuário
+main :-
+    write("Qual é o seu nível? (iniciante, intermediario, avancado): "), nl,
+    read(Nivel),
+    write("Qual grupo muscular deseja trabalhar? (superiores, inferiores, costas, peito, abdomen): "), nl,
+    read(GrupoMuscular),
+    recomendaExercicios(Nivel, GrupoMuscular).
